d56a4d36730e4fd6efeac3f6ef0865999789c07c
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-19 21:13:35 +0200

    created zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import CriptoSearchForm from "./components/CriptoSearchForm"
-
+import { useEffect } from "react";
+import CriptoSearchForm from "./components/CriptoSearchForm";
+import { useCryptoStore } from "./store";
 
 function App() {
+  const fetchCryptos = useCryptoStore((state) => state.fetchCryptos);
 
+  useEffect(() => {
+    fetchCryptos();
+  }, []);
   return (
-   <>
-    <div className="container">
-      <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
-      <div className="content">
-        <CriptoSearchForm/>
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+        <div className="content">
+          <CriptoSearchForm />
+        </div>
       </div>
-    </div>
-   </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
