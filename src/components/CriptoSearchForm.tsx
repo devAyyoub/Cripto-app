8b2596554ebc9d0f7308ed28dc732abb1372d40d
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-19 20:34:16 +0200

    created currency type and added currencies list to the bd

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,25 +1,37 @@
-import { useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react";
+import { currencies } from "../data";
 
 export default function CriptoSearchForm() {
+  const [currency, setCurrency] = useState("");
 
-    const [currency, setCurrency] = useState('')
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e : ChangeEvent<HTMLSelectElement>) => {
+    setCurrency(e.target.value)
+    console.log(currency)
+  }
 
   return (
-    <form action="" className="form">
-        <div className="field">
-            <label htmlFor="currency">Moneda:</label>
-            <select name="currency" id="currency">
-                <option value="">-- Seleccione --</option>
-            </select>
-        </div>
-        <div className="field">
-            <label htmlFor="criptocurrency">Criptomoneda:</label>
-            <select name="criptocurrency" id="criptocurrency">
-                <option value="">-- Seleccione --</option>
-            </select>
-        </div>
+    <form action="" className="form" onSubmit={handleSubmit}>
+      <div className="field">
+        <label htmlFor="currency">Moneda:</label>
+        <select name="currency" id="currency" onChange={handleChange}>
+          <option value="">-- Seleccione --</option>
+          {currencies.map((currency) => (
+            <option key={currency.code} value={currency.code}>{currency.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className="field">
+        <label htmlFor="criptocurrency">Criptomoneda:</label>
+        <select name="criptocurrency" id="criptocurrency">
+          <option value="">-- Seleccione --</option>
+        </select>
+      </div>
 
-        <input type="submit" value="Cotizar" />
+      <input type="submit" value="Cotizar" />
     </form>
-  )
+  );
 }
