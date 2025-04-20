619f2972a426db9987f756a5c01ac6d15d32d4a5
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-20 14:41:55 +0200

    refactor: move getCryptos function to a separate service file and update store integration

--- src/store.ts
+++ src/store.ts
@@ -1,32 +1,21 @@
 import { create } from "zustand";
-import axios from "axios";
 import { devtools } from "zustand/middleware";
-import { CryptoCurrenciesResponseSchema } from "./schema/cripto-schema";
 import { CryptoCurrency } from "./types";
+import { getCryptos } from "./services/CryptoService";
 
 type CryptoStore = {
-    cryptoCurrencies : CryptoCurrency[],
-    fetchCryptos: () => Promise<void>
-}
+  cryptoCurrencies: CryptoCurrency[];
+  fetchCryptos: () => Promise<void>;
+};
 
-async function getCryptos() {
-  const url =
-    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
-  const {
-    data: { Data },
-  } = await axios.get(url);
-  const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-  if (result.success) {
-    return result.data;
-  }
-}
-
-export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
-  cryptoCurrencies: [],
-  fetchCryptos: async () => {
-    const cryptoCurrencies = await getCryptos();
-    set(() => ({
-        cryptoCurrencies
-    }))
-  },
-})));
+export const useCryptoStore = create<CryptoStore>()(
+  devtools((set) => ({
+    cryptoCurrencies: [],
+    fetchCryptos: async () => {
+      const cryptoCurrencies = await getCryptos();
+      set(() => ({
+        cryptoCurrencies,
+      }));
+    },
+  }))
+);
