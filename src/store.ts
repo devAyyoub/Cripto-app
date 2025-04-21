cea5095592692cc31ac36c4ad082f4d23b59c76e
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-21 02:10:18 +0200

    refactor: add CryptoPrice type and enhance fetchData in store for better state management

--- src/store.ts
+++ src/store.ts
@@ -1,27 +1,30 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import { CryptoCurrency, Pair } from "./types";
+import { CryptoCurrency, CryptoPrice, Pair } from "./types";
 import { fetchCurrentCryptoPrice, getCryptos } from "./services/CryptoService";
 
 type CryptoStore = {
   cryptoCurrencies: CryptoCurrency[];
+  result: CryptoPrice;
   fetchCryptos: () => Promise<void>;
-  fetchData: (pair: Pair) => Promise<void>
+  fetchData: (pair: Pair) => Promise<void>;
 };
 
 export const useCryptoStore = create<CryptoStore>()(
   devtools((set) => ({
     cryptoCurrencies: [],
+    result: {} as CryptoPrice,
     fetchCryptos: async () => {
       const cryptoCurrencies = await getCryptos();
       set(() => ({
         cryptoCurrencies,
       }));
     },
-    fetchData : async (pair) => {
-        const result = await fetchCurrentCryptoPrice(pair)
-        console.log(result);
-        
-    }
+    fetchData: async (pair) => {
+      const result = await fetchCurrentCryptoPrice(pair);
+      set({
+        result,
+      });
+    },
   }))
 );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
-import { CryptoCurrencyResponseSchema, CurrencySchema, PairSchema } from "../schema/cripto-schema";
+import { CryptoCurrencyResponseSchema, CryptoPriceSchema, CurrencySchema, PairSchema } from "../schema/cripto-schema";
 
 export type Currency = z.infer<typeof CurrencySchema>
 
 export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
 
 export type Pair = z.infer<typeof PairSchema>
+
+export type CryptoPrice = z.infer<typeof CryptoPriceSchema>
