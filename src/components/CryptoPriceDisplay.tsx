4659faf562c41ea756a91bd019a3ade44ac8b308
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-21 18:44:19 +0200

    refactor: improve layout and styling of CryptoPriceDisplay component

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -3,20 +3,35 @@ import { useCryptoStore } from "../store";
 
 export default function CryptoPriceDisplay() {
   const result = useCryptoStore((state) => state.result);
-  const hasResult = useMemo(
-    () => !Object.values(result).includes(""),
-    [result]
-  );
+  const hasResult = useMemo(() => {
+    return result && Object.keys(result).length > 0 && result.PRICE;
+  }, [result]);
 
   return (
-    <div>
+    <div className="result-wrapper">
       {hasResult && (
         <>
           <h2>Cotización</h2>
           <div className="result">
+            <img
+              src={`https://cryptocompare.com/${result.IMAGEURL}`}
+              alt="Imagen criptomoneda"
+            />
             <div>
               <p>
-                El precio es de <span>{result.PRICE}</span>
+                El precio es de: <span>{result.PRICE}</span>
+              </p>
+              <p>
+                Precio más alto del dia: <span>{result.HIGHDAY}</span>
+              </p>
+              <p>
+                Precio más bajo del día: <span>{result.LOWDAY}</span>
+              </p>
+              <p>
+                Variación últimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span>
+              </p>
+              <p>
+                Última actualización: <span>{result.LASTUPDATE}</span>
               </p>
             </div>
           </div>
